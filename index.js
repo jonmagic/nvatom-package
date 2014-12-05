bf6bd9588d567114a278cf701ac356cf4ec2adfd
Jonathan Hoyt <hoyt@github.com>
2014-12-05 11:38:00 -0800

    Try sticking some elements on a page

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
+var DocumentStore = require("./lib/DocumentStore");
+var PathWatcher   = require("./lib/PathWatcher");
+
 var NVatom = {
   activate: function(state) {
+    atom.commands.add("atom-workspace", "nvatom:toggle", function() {
+      var workspaceElement = atom.views.getView(atom.workspace);
 
+      workspaceElement.style.display = "none";
+      var containerElement = document.createElement("div");
+      containerElement.setAttribute("id", "nvatom-container");
+      var searchElement = document.createElement("input");
+      searchElement.setAttribute("id", "search");
+      var documentListElement = document.createElement("ul");
+      documentListElement.setAttribute("id", "document-list");
+
+      containerElement.appendChild(searchElement);
+      containerElement.appendChild(documentListElement);
+      document.body.appendChild(containerElement);
+
+      var documentStore = new DocumentStore;
+      var pathEvents = {
+        update: function(details) {
+          documentStore.load(details);
+          var documentListItemElement = document.createElement("li");
+          documentListItemElement.textContent = details.filename;
+          containerElement.appendChild(documentListItemElement);
+        },
+        remove: function(filename) {
+          documentStore.remove(filename);
+        }
+      }
+
+      new PathWatcher(atom.project.getPath(), [".md"], pathEvents);
+    })
   },
 
   deactivate: function() {
@@ -11,3 +43,5 @@ var NVatom = {
 
   }
 }
+
+module.exports = NVatom;
