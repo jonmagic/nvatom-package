f797747f8ce95d09576384e687eb8d5ae992b81c
Jonathan Hoyt <hoyt@github.com>
2014-12-06 23:00:56 -0800

    Load files into DocumentStore and pass to App

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var React = require("atom").React;
 var App = require("./lib/components/App");
+var PathWatcher = require("./lib/PathWatcher");
+var DocumentStore = require("./lib/stores/DocumentStore");
 
 module.exports = {
   activate: function(state) {
@@ -15,7 +17,19 @@ module.exports = {
       nvAtomWorkspaceElement.setAttribute("id", "nvatom-workspace");
       document.body.appendChild(nvAtomWorkspaceElement);
 
-      React.renderComponent(App(), nvAtomWorkspaceElement);
+      var documentStore = new DocumentStore;
+      var pathEvents = {
+        update: function(details) {
+          documentStore.load(details);
+        },
+        remove: function(filename) {
+          documentStore.remove(filename);
+        }
+      }
+
+      new PathWatcher(atom.project.getPath(), [".md"], pathEvents);
+
+      React.renderComponent(App({documentStore: documentStore}), nvAtomWorkspaceElement);
     });
   },
 
