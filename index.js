e6f066c09fbcde0f3519a391f4799f8e4cc14f7f
Jonathan Hoyt <hoyt@github.com>
2014-12-19 14:28:41 -0800

    Disable packages instead of hiding atom workspace

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
+var App = require("./lib/App");
+
 module.exports = {
   activate: function(state) {
     atom.commands.add("atom-workspace", "nvatom:toggle", function() {
-      var workspaceElement = atom.views.getView(atom.workspace);
-      workspaceElement.style.display = "none";
-
-      var containerElement = document.createElement("div");
-      containerElement.setAttribute("id", "nvatom-container");
-      document.body.appendChild(containerElement);
-    })
+      if(App.active){
+        App.stop();
+      }else{
+        App.start();
+      }
+    });
   },
 
   deactivate: function() {
-
   },
 
   serialize: function() {
